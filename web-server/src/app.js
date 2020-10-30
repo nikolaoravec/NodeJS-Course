a53d60a97600ebb94960145904319038f0fd7918
nikolaoravec <oravec.nikola@gmail.com>
2020-10-30 12:30:38 +0100

    Added header and footer hbs and did a bit of styling

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,23 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs');
+const { registerPartials } = require('hbs');
 
 const app = express()
 
-const publicDirPath = path.join(__dirname, '../public')
 
+//Define paths for express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewaPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//Setup static directory to serve
 app.use(express.static(publicDirPath))
+
+//Setup handlebars engine and wievs location
 app.set('view engine','hbs')
+app.set('views', viewaPath)
+hbs.registerPartials(partialsPath)
 
 app.get('', (req,res) => {
     res.render('index', {
@@ -31,6 +42,20 @@ app.get('/help', (req,res) => {
 
 app.get('/weather', (req,res) => {
     res.send('Weather page')
+})
+
+app.get('/help/*', (req,res) => {
+    res.render('404', {
+        title: '404',
+        error: 'Help article not found'
+    })
+})
+
+app.get('*', (req,res) => {
+    res.render('404', {
+        title: '404',
+        error: 'Page not found'
+    })
 })
 
 app.listen(3000, () => {
