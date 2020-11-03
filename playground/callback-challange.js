f4a58860a099905a33b67d7c18fa96769594a6fa
nikolaoravec <oravec.nikola@gmail.com>
2020-11-03 19:23:54 +0100

    Setup mongodb npm, mongodb database with windows, From CRUD finished create update and read, learned promise for async working with handling results and errors.

--- playground/callback-challange.js
+++ playground/callback-challange.js
@@ -1,19 +1,27 @@
-//
-// Goal: Mess around with the callback pattern
-//
-// 1. Define an add function that accepts the correct arguments
-// 2. Use setTimeout to simulate a 2 second delay
-// 3. After 2 seconds are up, call the callback function with the sum
-// 4. Test your work!
-
-const add = (num1,num2,callback) => {
-    setTimeout( () => {
-        const add = num1 + num2
-        callback(add)
-    },1000)
-
+const doWorkCallBack = (callback) => {
+    setTimeout(() => {
+        // callback('This is my error', undefined)
+        callback(undefined,[1,4,7])
+    }, 2000);
 }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
+doWorkCallBack((error, reslut) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log(reslut);
 })
+
+
+
+// const add = (num1,num2,callback) => {
+//     setTimeout( () => {
+//         const add = num1 + num2
+//         callback(add)
+//     },1000)
+
+// }
+
+// add(1, 4, (sum) => {
+//     console.log(sum) // Should print: 5
+// })
