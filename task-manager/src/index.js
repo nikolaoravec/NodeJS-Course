95120f5fb54467600dbf92bc88bcec6402fa0172
nikolaoravec <oravec.nikola@gmail.com>
2020-12-14 19:22:24 +0100

    Serving up files, auto-cropping and image formatting

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,44 +2,14 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const multer = require('multer')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a word document.'))
-        }
-        cb(undefined, true)
-    }
-})
-
-app.post('/upload',upload.single('upload') , (req,res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({
-        error: error.message
-    })
-})
-
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-}
-
-main()
 app.listen(port, () => {
     console.log('Server is listening on port ' + port);
 })
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,6 +3,7 @@ const User = require("../models/user")
 const router = express.Router()
 const auth = require('../middleware/auth');
 const multer = require('multer');
+const sharp = require('sharp');
 
 const upload = multer({
     limits: {
@@ -36,6 +37,7 @@ router.patch('/users/me', auth,async (req,res) => {
             req.user[update] = req.body[update]
         })
         await req.user.save()
+        
         res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
@@ -61,6 +63,7 @@ router.post('/users',async (req, res) => {
 
     try {
         await user.save()
+
         res.status(201).send(user)
     } catch (error) {
         res.status(400).send(error)
@@ -71,6 +74,7 @@ router.post('/users/login', async (req,res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
+
         res.send({user, token})
     } catch (error) {
         res.status(400).send()
@@ -81,6 +85,7 @@ router.post('/users/logout' ,auth , async (req,res) => {
     try {
         req.user.tokens = req.user.tokens.filter((token) => {return token.token !== req.token})
      await req.user.save()
+
      res.send()
     } catch (error) {
         res.status(500).send()
@@ -100,7 +105,9 @@ router.post('/users/logoutAll' ,auth ,async (req,res) => {
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res) => {
     try {
-        req.user.avatar = req.file.buffer
+        const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+        req.user.avatar = buffer
+
         await req.user.save()
         res.send()
     } catch (error) {
@@ -113,10 +120,26 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res) =
 router.delete('/users/me/avatar', auth, async (req, res) => {
     try {
         req.user.avatar = undefined
+
         await req.user.save()
         res.send()
     } catch (error) {
         res.status(500).send(error)
+    }
+})
+
+router.get('/users/:id/avatar',async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch (error) {
+        res.status(404).send()
     }
 })
 
